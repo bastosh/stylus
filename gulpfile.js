e4494bd56b2963b9e8df26f700e9c81e5ab38039
Sébastien Pereda <sebastien.pereda@gmail.com>
2017-01-25 15:51:55 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+	plugins = require('gulp-load-plugins')(),
+	typographic = require('typographic'),
+	nib = require('nib'),
+	rupture = require('rupture'),
+	source = './styl/',
+	dest = 'css';
+
+gulp.task('stylus', function () {
+    return gulp.src(source+'main.styl')
+        .pipe(plugins.stylus({
+            use: [typographic(), nib(), rupture()]
+        }))
+        .pipe(plugins.cssbeautify({indent: '  '}))
+        .pipe(plugins.autoprefixer())
+        .pipe(gulp.dest(dest))
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(plugins.cssnano())
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(source+'*.styl', ['stylus']);
+});
+
+gulp.task('default', ['watch']);
